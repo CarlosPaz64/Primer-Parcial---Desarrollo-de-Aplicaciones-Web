e93b3ac0a86b33c863c91a057b7983b2ee0314d5
CarlosPaz64 <134614533+CarlosPaz64@users.noreply.github.com>
2024-10-03 22:05:04 -0600

    Arreglos finales

--- src/appBar/AppBar.tsx
+++ src/appBar/AppBar.tsx
@@ -13,7 +13,7 @@ const AppBar: React.FC = () => {
       }}
     >
       <h1 style={{ fontWeight: 'bold', margin: 0 }}>Aplicación de notas</h1>
-      <h2 style={{ fontWeight: 400, margin: 0 }}>Bienvenido. Esta es una aplicación de notas horizontal. Al crear un espacio, lo podrás encontrar moviendo el conjunto de notas o tus notas horizontalmente.</h2>
+      <h2 style={{ fontWeight: 400, margin: 0 }}>Bienvenido. Esta es una aplicación de notas horizontal. Al crear un espacio, lo podrás encontrar moviendo el conjunto de notas o tus notas horizontalmente. Puedes cambiar el nombre de los espacios dándole click a su nombre.</h2>
     </header>
   );
 };
